24f7d484cd028ee2fc025d7a8a73c2461e204fcd
secolimae <secolimae@berea.edu>
2025-01-13 11:32:29 -0500

    -Added art -Deleted TODO commands

--- main.cpp
+++ main.cpp
@@ -5,9 +5,7 @@
     Original code and ASCII art by Dr. Jan Pearce, Berea College
 
     Edits made by:
-    TODO: <Add name of student 1 Here> -- to be added by student 1
-    TODO: <Add name of student 2 Here> -- to be added by student 2
-    TODO: <Add name of student 3 Here> -- to be added by student 3 (if relevant otherwise, student 2 should remove this line)
+    Eber Seco Lima
  */
 #include <iostream>
 using namespace std;
@@ -21,11 +19,25 @@ int main() {
     cout << " How many rubber ducks do you want to see? ";
     cin >> ducknum;
 
-    for (int i = 0; i < ducknum; i++) { 
-        cout << "                __     " << endl; // TODO: student 1 may change this line and may add additional lines immediately above
-        cout << "              <(o )___-" << endl; // student 3(1) may change this line and add additional lines between this line and immediately below
-        cout << "               ( .__> /" << endl; // student 3(2) may change this line
-        cout << "                `----' " << endl; // TODO student 2 may change this line and may add lines below
+    for (int i = 0; i < ducknum; i++) {
+        
+        cout << "  ____________________________________ " << endl;
+		cout << " |\\                                    \\ " << endl;
+		cout << " | \\                                    \\ " << endl;
+		cout << " |  \\____________________________________\\ " << endl;
+		cout << " |  |       __---_ _---__                | " << endl;
+		cout << " |  |      |======|=====|                | " << endl;
+		cout << " |  |      |======|=====|                | " << endl;
+		cout << " |  |  ____|__---_|_---_|______________  | " << endl;
+		cout << " |  | |                                | | " << endl;
+		cout << " |   \\ \\                                \\ \\ " << endl;
+		cout << " |  \\ ||\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\| |" << endl;
+		cout << " |  |\\  ,--------------------------------  | " << endl;
+		cout << " |  ||| |                               || | " << endl;
+		cout << "  \\ ||| |           -  -                || | " << endl;
+		cout << "   \\'|| |-----------\\-\\---------------|| | " << endl;
+		cout << "     \\|_|            "  "               \\|_| " << endl;
+
     }
 
     return 0;
