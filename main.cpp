2d43e1e5ad5c457096bea6faddfd3a6a3c902af3
secolimae <secolimae@berea.edu>
2025-01-15 11:30:26 -0500

    -Adding changes

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 /*  T02: Collaboration Teamwork
 
-    Have yov ever heard of rubber duck debugging?
-    See https://en.wikipedia.org/wiki/Rubber_duck_debugging 
-    Original code and ASCII art by Dr. Jan Pearce, Berea College
+	Have yov ever heard of rubber duck debugging?
+	See https://en.wikipedia.org/wiki/Rubber_duck_debugging
+	Original code and ASCII art by Dr. Jan Pearce, Berea College
 
-    Edits made by:
-    Eber Seco Lima
+	Edits made by:
+	Eber Seco Lima
+	https://github.com/berea-college-csc236/t02-collaboration-keplar
  */
 #include <iostream>
 using namespace std;
 
 int main() {
 
-    int ducknum;
+	int ducknum;
 
-    cout << " Have you ever hver heard of rubber duck debugging?" << endl;
-    cout  << " If not, see See https://en.wikipedia.org/wiki/Rubber_duck_debugging.\n" << endl;
-    cout << " How many rubber ducks do you want to see? ";
-    cin >> ducknum;
+	cout << " Have you ever hver heard of rubber duck debugging?" << endl;
+	cout << " If not, see See https://en.wikipedia.org/wiki/Rubber_duck_debugging.\n" << endl;
+	cout << " How many rubber ducks do you want to see? ";
+	cin >> ducknum;
 
-    for (int i = 0; i < ducknum; i++) {
-        
-        cout << "  ____________________________________ " << endl;
+	for (int i = 0; i < ducknum; i++) {
+
+		cout << "  ____________________________________ " << endl;
 		cout << " |\\                                    \\ " << endl;
 		cout << " | \\                                    \\ " << endl;
 		cout << " |  \\____________________________________\\ " << endl;
@@ -38,7 +39,7 @@ int main() {
 		cout << "   \\'|| |-----------\\-\\---------------|| | " << endl;
 		cout << "     \\|_|            "  "               \\|_| " << endl;
 
-    }
+	}
 
-    return 0;
+	return 0;
 }
